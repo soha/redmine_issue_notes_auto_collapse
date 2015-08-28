5c400e3ba52b9ff30694fed7297149cf8f86e958
sohatach <sohatach@gmail.com>
2015-08-28 17:06:25 +0900

    impl change injection method

--- app/helpers/issue_notes_auto_collapse_helper.rb
+++ app/helpers/issue_notes_auto_collapse_helper.rb
@@ -5,27 +5,95 @@
 
 module IssueNotesAutoCollapseHelper
 
-  def deco_collapse(str)
-    return str if str.nil?
+
+  INAC_QUOTES_RE = /(^>+([^\n]*?)(\n|$))+/m
+  INAC_QUOTES_CONTENT_RE = /^([> ]+)(.*)$/m
   
-    sidx = str.index("<blockquote>")
-    return print str if sidx.nil?
-  
-    rep_str = str[0..sidx]
-    rep_str += "{{collapse" 
-    rep_str += str[sidx..str.length]
-  
-    rep_str2 = "" 
-    eidx = rep_str.rindex("</blockquote>")
-    return print str if eidx.nil?
-  
-    eidx += "</blockquote>".length
-    rep_str2 = rep_str[0..eidx]
-    rep_str2 += "}}" 
-    rep_str2 += rep_str[eidx+"}}".length..rep_str.length]
-  
-    print rep_str2
+  def deco_block_textile_quotes( text )
+    text.gsub!( INAC_QUOTES_RE ) do |match|
+      lines = match.split( /\n/ )
+      quotes = ''
+      indent = 0
+      collapse = false
+
+      lines.each do |line|
+        line =~ INAC_QUOTES_CONTENT_RE 
+        bq,content = $1, $2
+        l = bq.count('>')
+
+        if l > 0 and not collapse then
+          quotes << ("\n\n" + '{{collapse' + "\n\n")
+          collapse = true
+        end
+
+        if l != indent then
+          quotes << ("\n\n" + (l>indent ? '>' * (l-indent) : '' * (indent-l) + "\n\n"))
+        end
+        quotes << (content + "\n")
+      end
+
+      quotes << ("\n" + '' * indent + "\n\n")
+
+      if collapse then
+        quotes << ("\n" + '}}' "\n\n")
+      end
+
+      quotes
+    end
   end
+
+
+  # from application_helper.rb
+  #
+  # Formats text according to system settings.
+  # 2 ways to call this method:
+  # * with a String: textilizable(text, options)
+  # * with an object and one of its attribute: textilizable(issue, :description, options)
+  def deco_textilizable(*args)
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    case args.size
+    when 1
+      obj = options[:object]
+      text = args.shift
+    when 2
+      obj = args.shift
+      attr = args.shift
+      text = obj.send(attr).to_s
+    else
+      raise ArgumentError, 'invalid arguments to textilizable'
+    end
+    return '' if text.blank?
+    project = options[:project] || @project || (obj && obj.respond_to?(:project) ? obj.project : nil)
+    only_path = options.delete(:only_path) == false ? false : true
+
+    text = text.dup
+
+    ####add custom
+    text = deco_block_textile_quotes(text)
+    ####
+
+    macros = catch_macros(text)
+    text = Redmine::WikiFormatting.to_html(Setting.text_formatting, text, :object => obj, :attribute => attr)
+
+    @parsed_headings = []
+    @heading_anchors = {}
+    @current_section = 0 if options[:edit_section_links]
+
+    parse_sections(text, project, obj, attr, only_path, options)
+    text = parse_non_pre_blocks(text, obj, macros) do |text|
+      [:parse_inline_attachments, :parse_wiki_links, :parse_redmine_links].each do |method_name|
+        send method_name, text, project, obj, attr, only_path, options
+      end
+    end
+    parse_headings(text, project, obj, attr, only_path, options)
+
+    if @parsed_headings.any?
+      replace_toc(text, @parsed_headings)
+    end
+
+    text.html_safe
+  end
+
 
   def render_notes_auto_collapse(issue, journal, options={})
     content = ''
@@ -42,10 +110,14 @@ module IssueNotesAutoCollapseHelper
                                                 :title => l(:button_edit)) if editable
     end
     content << content_tag('div', links.join(' ').html_safe, :class => 'contextual') unless links.empty?
-    content << textilizable(journal, :notes)
+
+    ####mod custom
+    content << deco_textilizable(journal, :notes)
+    ####
+
     css_classes = "wiki"
     css_classes << " editable" if editable
-    content_tag('div', deco_collapse(content.html_safe), :id => "journal-#{journal.id}-notes", :class => css_classes)
+    content_tag('div', content.html_safe, :id => "journal-#{journal.id}-notes", :class => css_classes)
   end
 
 end
--- init.rb
+++ init.rb
@@ -3,6 +3,13 @@ Redmine::Plugin.register :issue_notes_auto_collapse do
   author 'sohatach'
   description 'This is a plugin for Redmine 2.5.3'
   version '0.0.1'
-  url 'http://github.com/soha/IssueNotesAutoCollapsePlugin'
-  author_url 'http://github.com/soha'
+  url 'https://github.com/soha/IssueNotesAutoCollapsePlugin'
+  author_url 'https://github.com/soha'
 end
+
+Rails.configuration.to_prepare do
+  unless JournalsHelper.included_modules.include?(IssueNotesAutoCollapseHelper)
+    JournalsHelper.send(:include, IssueNotesAutoCollapseHelper)
+  end
+end
+
