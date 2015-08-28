3ae9a6eedb82005a392d479164548695bf5579a8
sohatach <sohatach@gmail.com>
2015-08-28 10:52:05 +0900

    mod project info

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :issue_notes_auto_collapse do
   name 'Issue Notes Auto Collapse plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'sohatach'
+  description 'This is a plugin for Redmine 2.5.3'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'http://github.com/soha/IssueNotesAutoCollapsePlugin'
+  author_url 'http://github.com/soha
 end
