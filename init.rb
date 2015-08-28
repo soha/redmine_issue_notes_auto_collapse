1584b4ccb73444a6c1b8d2a2b1beba2768688172
sohatach <sohatach@gmail.com>
2015-08-28 12:28:45 +0900

    fix typo

--- init.rb
+++ init.rb
@@ -4,5 +4,5 @@ Redmine::Plugin.register :issue_notes_auto_collapse do
   description 'This is a plugin for Redmine 2.5.3'
   version '0.0.1'
   url 'http://github.com/soha/IssueNotesAutoCollapsePlugin'
-  author_url 'http://github.com/soha
+  author_url 'http://github.com/soha'
 end
